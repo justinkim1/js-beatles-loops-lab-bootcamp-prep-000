91ac2dc82d1a1277bb35342e3b1955e719b3e1c7
Justin K <justin-kim1@hotmail.com>
2017-12-22 05:12:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function theBeatlesPlay(musicians, instruments){
 }
 function johnLennonFacts(facts)
   array = [];
-  i = 0;
+  var i = 0;
   while(i < facts.length){
     array.push(facts[i] + '!!!');
     i++;
